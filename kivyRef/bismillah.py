80f248419a6e96beb41cec60615fcefc61d3da05
thesharifdev <sharif.golang@gmail.com>
2023-08-06 23:36:32 +0600

    bismillah file edited

--- kivyRef/bismillah.py
+++ kivyRef/bismillah.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return(Label(text="Bismillah"))
+        return(Label(text="Bismillah! edited"))
     
 if __name__ == "__main__":
     MyApp().run()
